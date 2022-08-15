e613f3b81e9ea592da996ec78d9ea5ebd037d156
Dark-WizZ <mailwithakram@gmail.com>
2022-08-15 13:57:49 +0530

    Add function to add boks to the view

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let library = [];
 
-const bookName = document.querySelector('.name');
-const author = document.querySelector('.author');
-const pages = document.querySelector('.pages');
-const isRead = document.querySelector('.isRead');
+const bookNameIP = document.querySelector('#bookName');
+const authorIP = document.querySelector('#author');
+const pagesIP = document.querySelector('#pages');
+const isReadIP = document.querySelector('#isRead');
+let submitIP = document.querySelector('.submit');
+const cards = document.querySelector('.cards');
 const add = document.querySelector('.add');
+const input = document.querySelector('.input');
+const card = document.querySelector('.cards');
 
-add.addEventListener('click', addBook);
+submitIP.addEventListener('click', addBook);
 
 function Book (name, author, pages, isRead){
   this.name = name;
@@ -16,10 +20,18 @@ function Book (name, author, pages, isRead){
 }
 
 function addBook(){
-  const _bookName = bookName.value;
-  const _author = author.value;
-  const _pages = pages.value;
-  const _isRead = isRead.value;
+  let _bookName = bookNameIP.value;
+  let _author = authorIP.value;
+  let _pages = pagesIP.value;
+  let _isRead = (isReadIP.checked)?'read':'';
   library.push(new Book (_bookName, _author, _pages, _isRead));
   console.table(library)
+
+  input.insertAdjacentHTML('afterend', 
+            `<div class="card">
+              <div class="bookName">${_bookName}</div>
+              <div class="author">-${_author}</div>
+              <div class="pages">Pages: ${_pages}</div>
+              <div class="isRead">${(_isRead)?'Read':'Unread'}</div>
+            </div>`)
 }
